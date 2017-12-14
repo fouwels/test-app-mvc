c283f38895f4e9186333fe926d75b7130bb25d25
Kaelan Fouwels <kf@kaelanfouwels.com>
2017-12-14 15:28:20 +0000

    all

---
+++ test-app-mvc/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using InterFAX.Api;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test_app_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            await SendInterFax();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public async Task SendInterFax()
+        {
+            // Update username/password in web.config to valid credentials first. Otherwise will throw 401:unauthorised
+
+            var username = ConfigurationManager.AppSettings["INTERFAX_USERNAME"];
+            var password = ConfigurationManager.AppSettings["INTERFAX_PASSWORD"];
+            FaxClient fc = new FaxClient(username, password);
+            string filePath = System.Web.HttpContext.Current.Server.MapPath("sample.pdf");
+            IFaxDocument ifd = fc.Documents.BuildFaxDocument(filePath);
+            SendOptions so = new SendOptions();
+            so.FaxNumber = "+99999990";
+            so.PageHeader = "test";
+            try
+            {
+                var result = await fc.Outbound.SendFax(ifd, so);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
---
+++ test-app-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(test_app_mvc.Startup))]
+namespace test_app_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
